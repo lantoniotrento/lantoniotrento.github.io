63c10addaec7059df5cc357ceeaa098b61c76491
Antonio Trento <antonio.trento@yahoo.com>
2017-09-29 15:39:42 +0200

    Update type.js

--- assets/js/type.js
+++ assets/js/type.js
@@ -1,30 +1,29 @@
-jQuery(document).ready(function($){
+(function() {
+  var app;
 
-  'use strict';
+  $(document).ready(function() {
+    return app.init();
+  });
 
-// Jazz'd up v2 is in https://codepen.io/ronniechong/pen/PzVKOo
+  app = {
+    text: "Hello stranger. How are you doing?",
+    index: 0,
+    chars: 0,
+    speed: 100,
+    container: ".text .content",
+    init: function() {
+      this.chars = this.text.length;
+      return this.write();
+    },
+    write: function() {
+      $(this.container).append(this.text[this.index]);
+      if (this.index < this.chars) {
+        this.index++;
+        return window.setTimeout(function() {
+          return app.write();
+        }, this.speed);
+      }
+    }
+  };
 
-var items = ['Playing', 'with', 'Codepen', 'is so much', 'funz!'];
-var app = document.getElementById('app');
-var count = 0;
-var index = 0;
-var typingEffect = function typingEffect() {
-  var text = items[index];
-  if (count < text.length) {
-    setTimeout(function () {
-      app.innerHTML += text[count];
-      count++;
-      typingEffect();
-    }, Math.floor(Math.random(10) * 100));
-  } else {
-    count = 0;
-    index = index + 1 < items.length ? index + 1 : 0;
-    setTimeout(function () {
-      app.innerHTML = '';
-      typingEffect();
-    }, 1500);
-  }
-};
-
-typingEffect();  
-});
+}).call(this);
